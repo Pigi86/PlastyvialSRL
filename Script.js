e03627f7bfa457da992b039af10eb3f5c8da84b8
Pigi86 <41274346+Pigi86@users.noreply.github.com>
2026-06-07 19:06:31 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }, 50);
     }
 
-    const menuLinks = document.querySelectorAll('nav a[href^="#"], .footer-links a, #solicitarBtn, #verProdBtn, .backServices, .serviceGridLink');
+    const menuLinks = document.querySelectorAll('nav a[href^="#"], .footer-links a, #solicitarBtn, #verProdBtn, .backServicesLeft, .backServices, .serviceGridLink');
 
     menuLinks.forEach(link => {
         link.addEventListener("click", function (e) {
